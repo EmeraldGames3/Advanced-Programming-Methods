7e7bb351193e9fa02926afe4cd382adb3a926c55
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-10-19 21:12:48 +0300

    Ex2 solved + tests

--- Labor/Labor1/src/Ex2/Ex2.java
+++ Labor/Labor1/src/Ex2/Ex2.java
@@ -1,4 +1,75 @@
 package Ex2;
 
 public class Ex2 {
+    // Find the maximum number in the array
+    public static int findMax(int[] numbers) {
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        int max = numbers[0];
+
+        for (int number : numbers) {
+            // If the current number is greater than max, update max
+            max = Math.max(number, max);
+        }
+
+        return max;
+    }
+
+    // Find the minimum number in the array
+    public static int findMin(int[] numbers) {
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        int min = numbers[0];
+
+        for (int number : numbers) {
+            // If the current number is less than min, update min
+            min = Math.min(number, min);
+        }
+
+        return min;
+    }
+
+    // Find the maximum sum of n-1 numbers
+    public static int findMaxSumOfNMinusOne(int[] numbers) {
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        int sum = 0;
+        int min = numbers[0];
+
+        for (int number : numbers) {
+            // If the current number is less than min, update min
+            min = Math.min(number, min);
+            // Add the current number to the sum
+            sum += number;
+        }
+
+        // Subtract the minimum number from the sum
+        return sum - min;
+    }
+
+    // Find the minimum sum of n-1 numbers
+    public static int findMinSumOfNMinusOne(int[] numbers) {
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        int sum = 0;
+        int max = numbers[0];
+
+        for (int number : numbers) {
+            // If the current number is greater than max, update max
+            max = Math.max(number, max);
+            // Add the current number to the sum
+            sum += number;
+        }
+
+        // Subtract the maximum number from the sum
+        return sum - max;
+    }
 }
---
+++ Labor/Labor1/src/Ex2/Ex2Test.java
@@ -0,0 +1,117 @@
+package Ex2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Ex2Test {
+
+    private static final String TEST_ONE = "Biggest Number";
+    private static final String TEST_TWO = "Smallest Number";
+    private static final String TEST_THREE = "Maximum n-1 Sum";
+    private static final String TEST_FOUR = "Minimum n-1 Sum";
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void testFindBiggestNr() {
+        int[] arr = {1, 2, -3, 4, 5, 124};
+        int expected = 124;
+        int result = Ex2.findMax(arr);
+
+        try {
+            Assertions.assertEquals(expected, result);
+            System.out.println(TEST_ONE + " was successful");
+        } catch (AssertionError e) {
+            System.out.println(TEST_ONE + " has failed");
+        }
+    }
+
+    @Test
+    public void testFindBiggestNrFail() {
+        int[] arr = {};
+        try {
+            int result = Ex2.findMax(arr);
+            System.out.println(TEST_ONE + " has failed");
+        } catch (IllegalArgumentException e) {
+            System.out.println(TEST_ONE + " was successful");
+        }
+    }
+
+    @Test
+    public void testFindMinNumber() {
+        int[] arr = {1, 2, -3, 4, 5, 124};
+        int expected = -3;
+        int result = Ex2.findMin(arr);
+
+        try {
+            Assertions.assertEquals(expected, result);
+            System.out.println(TEST_TWO + " was successful");
+        } catch (AssertionError e) {
+            System.out.println(TEST_TWO + " has failed");
+        }
+    }
+
+    @Test
+    public void testFindMinNumberFail() {
+        int[] arr = {};
+        try {
+            int result = Ex2.findMin(arr);
+            System.out.println(TEST_TWO + " has failed");
+        } catch (IllegalArgumentException e) {
+            System.out.println(TEST_TWO + " was successful");
+        }
+    }
+
+    @Test
+    public void testMaxSum() {
+        int[] arr = {4, -1, 3};
+        int expected = 7;
+        int result = Ex2.findMaxSumOfNMinusOne(arr);
+
+        try {
+            Assertions.assertEquals(expected, result);
+            System.out.println(TEST_THREE + " was successful");
+        } catch (AssertionError e) {
+            System.out.println(TEST_THREE + " has failed");
+        }
+    }
+
+    @Test
+    public void testMaxSumFail() {
+        int[] arr = {};
+        try {
+            int result = Ex2.findMaxSumOfNMinusOne(arr);
+            System.out.println(TEST_THREE + " has failed");
+        } catch (IllegalArgumentException e) {
+            System.out.println(TEST_THREE + " was successful");
+        }
+    }
+
+    @Test
+    public void testMinSum() {
+        int[] arr = {4, -1, 3};
+        int expected = 2;
+        int result = Ex2.findMinSumOfNMinusOne(arr);
+
+        try {
+            Assertions.assertEquals(expected, result);
+            System.out.println(TEST_FOUR + " was successful");
+        } catch (AssertionError e) {
+            System.out.println(TEST_FOUR + " has failed");
+        }
+    }
+
+    @Test
+    public void testMinSumFail() {
+        int[] arr = {};
+        try {
+            int result = Ex2.findMinSumOfNMinusOne(arr);
+            System.out.println(TEST_FOUR + " has failed");
+        } catch (IllegalArgumentException e) {
+            System.out.println(TEST_FOUR + " was successful");
+        }
+    }
+}
